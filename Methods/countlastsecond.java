918834f57e85f722da62117d8596b56d1c7777a0
Nishiraj Singh <nishirajsingh2005@gmail.com>
2026-05-25 09:23:10 +0530

    Updated Modified Constraint

--- Methods/countlastsecond.java
+++ Methods/countlastsecond.java
@@ -2,13 +2,13 @@
 import java.util.Scanner;
 
 public class countlastsecond{
-    static int lastsecond(int n){
+    static long lastsecond(long n){
         if (n<9) return 0;
         return (n/10)%10;
     }
     public static void main(String[] args){
         Scanner sc =new Scanner(System.in);
-        int n = sc.nextInt();
+        long n = sc.nextLong();
         System.out.println(lastsecond(n));
     }
 }
