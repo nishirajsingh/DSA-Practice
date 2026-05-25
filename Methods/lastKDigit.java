3b6629172b8f19c02479b186054d102b8347cb28
Nishiraj Singh <nishirajsingh2005@gmail.com>
2026-05-25 09:36:48 +0530

    add new method

---
+++ Methods/lastKDigit.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class lastKDigit{
+    static long lastsecond(long n,int k){ // return last Kth digit
+        return (n/(long)Math.pow(10, k))%10;
+        
+    }
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        long n = sc.nextLong();
+        int k = sc.nextInt();
+        System.out.println(lastsecond(n,k));
+    }
+}
